af2446202071b255298b83ed27cc4a8dedb8f342
Henrik Valter Vogelius Hansson <groogy@groogy.se>
2017-09-24 22:01:16 +0200

    Playing around with Boleite::Text to test it out

--- src/ego/game_state.cr
+++ src/ego/game_state.cr
@@ -18,32 +18,15 @@ class GameState < Boleite::State
     texture2.create(texture.size.x, texture.size.y, texture.format, texture.type)
     texture2.update(texture)
     @sprite = Boleite::Sprite.new texture
-    @sprite.position = Boleite::Vector2f.new 300.0, 300.0
+    @sprite.position = Boleite::Vector2f.new 500.0, 600.0
     @sprite.origo = texture.size.to_f / 2.0
     @sprite.scale = Boleite::Vector2f.new 2.0, 2.0
     @sprite.rotation = 33.0
-    #@sprite.texture_rect = Boleite::IntRect.new(0, 0, 100, 50)
 
-    size = 129u32
-    @font = Boleite::Font.new(app.graphics, "/usr/share/fonts/TTF/arial.ttf")
-    puts "RENDERING 'a'"
-    @font.get_glyph('a', size)
-    puts "RENDERING 'b'"
-    @font.get_glyph('b', size)
-    puts "RENDERING 'c'"
-    @font.get_glyph('c', size)
-    puts "RENDERING 'd'"
-    @font.get_glyph('d', size)
-    puts "RENDERING 'e'"
-    @font.get_glyph('e', size)
-    puts "RENDERING 'f'"
-    @font.get_glyph('f', size)
-    @font.get_glyph('g', size)
-    puts "RENDERING 'h'"
-    @font.get_glyph('h', size)
-    puts "RENDERING 'g'"
-
-    @sprite2 = Boleite::Sprite.new @font.@pages[size].texture
+    @font = Boleite::Font.new app.graphics, "/usr/share/fonts/TTF/arial.ttf"
+    @text = Boleite::Text.new @font, "Hello world!\nNew line?"
+    @text.size = 150u32
+    @text.position = Boleite::Vector2f.new 10.0, 10.0
 
     @input = nil
   end
@@ -65,6 +48,6 @@ class GameState < Boleite::State
   def render(delta, renderer)
     @sprite.rotate delta.to_f * 10
     renderer.draw @sprite
-    renderer.draw @sprite2
+    renderer.draw @text
   end
 end
