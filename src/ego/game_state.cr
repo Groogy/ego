39027c98c9141e533676f14479f90f708be60d13
Henrik Valter Vogelius Hansson <groogy@groogy.se>
2017-10-15 00:32:11 +0200

    Started working on World class, moved some stuff there

--- src/ego/game_state.cr
+++ src/ego/game_state.cr
@@ -17,8 +17,8 @@ class GameState < Boleite::State
 
     tileset = Boleite::Texture.load_file "tileset.png", gfx
     tileset.smooth = false
-    @tilemap = Tilemap.new Boleite::Vector2u.new(64u32, 64u32), tileset
-    generate_tilemap
+    @world = World.new tileset
+    @world.generate_tilemap
 
     @input = nil
 
@@ -26,38 +26,6 @@ class GameState < Boleite::State
     @fps_text = Boleite::Text.new @font, "FPS:"
     @fps_text.formatter.add /(\d+)/, Boleite::Color.yellow
     @fps_text.size = 24u32
-  end
-
-  def generate_tilemap
-    size = 64u32
-    water = TileType.new "water", "Water", Boleite::Vector2u.new(1u32, 1u32)
-    plains = TileType.new "plains", "Plains", Boleite::Vector2u.new(19u32, 1u32)
-    mountains = TileType.new "mountains", "Mountains", Boleite::Vector2u.new(37u32, 1u32)
-    @tilemap.add_tile_type water
-    @tilemap.add_tile_type plains
-    @tilemap.add_tile_type mountains
-
-    center = Boleite::Vector2f.new size / 2.0 - 1, size / 2.0 - 1
-    size.times do |x|
-      size.times do |y|
-        delta = Boleite::Vector2f.new center.x - x, center.y - y
-        distance = Boleite::Vector.magnitude delta
-        coord = Boleite::Vector2u.new x, y
-        if distance < 12
-          @tilemap.set_tile coord, mountains
-        elsif distance < 30
-          @tilemap.set_tile coord, plains
-        else
-          @tilemap.set_tile coord, water
-        end
-
-        if distance < 14
-          height = 6u16 - distance.to_u16 / 2
-          @tilemap.set_tile_height coord, height, distance != 0 && distance.to_u16 % 2 == 0
-        end
-      end
-    end
-    @tilemap.cleanup_ramps
   end
 
   def enable
@@ -76,6 +44,8 @@ class GameState < Boleite::State
       input.update delta
     end
 
+    @world.update
+
     @fps_counter += 1
     @fps_timer += delta.to_f
     if @fps_timer > 1.0
@@ -88,7 +58,7 @@ class GameState < Boleite::State
   def render(delta)
     @renderer.clear Boleite::Color.black
     @renderer.camera = @camera3d
-    @tilemap.render @renderer
+    @world.render @renderer
     @renderer.camera = @camera2d
     @renderer.draw @fps_text
     @renderer.present
