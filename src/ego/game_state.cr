14d44eef25a69e571bf7ef57c222bc47e3209ed0
Henrik Valter Vogelius Hansson <groogy@groogy.se>
2017-10-14 21:32:45 +0200

    Added frame counter

--- src/ego/game_state.cr
+++ src/ego/game_state.cr
@@ -81,16 +81,19 @@ class GameState < Boleite::State
 
   @input : InputHandler | Nil
   @renderer : Boleite::Renderer
+  @fps_timer = 0.0
+  @fps_counter = 0
   
   def initialize(@app : EgoApplication)
     super()
 
     gfx = @app.graphics
     target = gfx.main_target
-    @camera = Boleite::Camera3D.new(60.0f32, target.width.to_f32, target.height.to_f32, 0.01f32, 100.0f32)
+    @camera3d = Boleite::Camera3D.new(60.0f32, target.width.to_f32, target.height.to_f32, 0.01f32, 100.0f32)
+    @camera2d = Boleite::Camera2D.new(target.width.to_f32, target.height.to_f32, 0f32, 1f32)
     shader = Boleite::Shader.load_file "test.shader", gfx
-    @renderer = Boleite::ForwardRenderer.new gfx, @camera, shader
-    @camera.move 0.0, 8.0, -2.5
+    @renderer = Boleite::ForwardRenderer.new gfx, @camera3d, shader
+    @camera3d.move 0.0, 8.0, -2.5
 
     tileset = Boleite::Texture.load_file "tileset.png", gfx
     tileset.smooth = false
@@ -98,6 +101,10 @@ class GameState < Boleite::State
     generate_tilemap
 
     @input = nil
+
+    @font = Boleite::Font.new gfx, "arial.ttf"
+    @fps_text = Boleite::Text.new @font, "FPS:"
+    @fps_text.size = 24u32
   end
 
   def generate_tilemap
@@ -133,7 +140,7 @@ class GameState < Boleite::State
   end
 
   def enable
-    input = InputHandler.new(@camera)
+    input = InputHandler.new(@camera3d)
     @app.input_router.register(input)
     @input = input
   end
@@ -152,13 +159,24 @@ class GameState < Boleite::State
       vector.x += 5.0 * delta.to_f if input.is_moving? :right
       vector.y -= 5.0 * delta.to_f if input.is_moving? :down
       vector.y += 5.0 * delta.to_f if input.is_moving? :up
-      @camera.move vector
+      @camera3d.move vector
+    end
+
+    @fps_counter += 1
+    @fps_timer += delta.to_f
+    if @fps_timer > 1.0
+      @fps_text.text = "FPS: #{@fps_counter}"
+      @fps_timer -= 1.0
+      @fps_counter = 0
     end
   end
 
   def render(delta)
     @renderer.clear Boleite::Color.black
+    @renderer.camera = @camera3d
     @tilemap.render @renderer
+    @renderer.camera = @camera2d
+    @renderer.draw @fps_text
     @renderer.present
   end
 end
