1a2457f701731b66795a8c617d6a72c39a74bce8
Henrik Valter Vogelius Hansson <groogy@groogy.se>
2018-03-19 22:39:26 +0100

    Menu state should not have active gui

--- src/ego/menu_state.cr
+++ src/ego/menu_state.cr
@@ -9,8 +9,18 @@ class MenuState < Boleite::State
     @camera2d = Boleite::Camera2D.new(target.width.to_f32, target.height.to_f32, 0f32, 1f32)
     shader = Boleite::Shader.load_file "test.shader", gfx
     @renderer = Boleite::ForwardRenderer.new gfx, @camera2d, shader
+    
     @gui = Boleite::GUI.new gfx, @app.input_router
+    @window = Boleite::GUI::Window.new
     build_gui target.size
+  end
+
+  def enable
+    @gui.add_root @window
+  end
+
+  def disable
+    @gui.remove_root @window
   end
 
   def update(delta)
@@ -45,8 +55,7 @@ class MenuState < Boleite::State
   end
 
   def build_gui(size)
-    window = Boleite::GUI::Window.new
-    window.header_text = "Main Menu"
+    @window.header_text = "Main Menu"
     container = Boleite::GUI::Layout.new :vertical
     
     button = Boleite::GUI::Button.new "New World", Boleite::Vector2f.new(200.0, 24.0)
@@ -61,9 +70,8 @@ class MenuState < Boleite::State
     button.click.on { quit }
     container.add button
 
-    window.add container
-    window_size = window.size
-    window.position = Boleite::Vector2f.new size.x / 2 - window_size.x / 2, size.y / 2 - window_size.y / 2
-    @gui.add_root window
+    @window.add container
+    window_size = @window.size
+    @window.position = Boleite::Vector2f.new size.x / 2 - window_size.x / 2, size.y / 2 - window_size.y / 2
   end
 end
