9d3e75c795da4876233048e775c9156095f35f81
Henrik Valter Vogelius Hansson <groogy@groogy.se>
2018-06-10 00:18:57 +0200

    Added satisfaction and getters to hunger component

--- src/ego/entity/components/hunger.cr
+++ src/ego/entity/components/hunger.cr
@@ -1,5 +1,23 @@
 class HungerComponent < EntityComponent
+  @satisfaction : Int64
+
+  property satisfaction
+
   def initialize(data, entity, world)
     super data, entity, world
+    @satisfaction = data.get_int "max_satisfaction"
+    pp edible
+  end
+
+  def satisfaction_decay
+    @data.get_int "satisfaction_decay"
+  end
+
+  def max_satisfaction
+    @data.get_int "max_satisfaction"
+  end
+
+  def edible
+    @data.get_array("edible").map { |val| val.as(String) }
   end
 end
