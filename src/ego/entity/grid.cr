f8ac06dd9207b6f316846c823a3f76a0d7db1080
Groogy <groogy@groogy.se>
2018-07-22 00:56:58 +0200

    Grid can now see if area is within it's limits

--- src/ego/entity/grid.cr
+++ src/ego/entity/grid.cr
@@ -38,7 +38,7 @@ class EntityGrid
     @grid = Array.new(count) { Bucket.new }
   end
 
-  requires inside? pos
+  requires inside? pos, entity.template.size
   requires !includes? entity
   ensures includes? entity, pos
   def add(entity, pos)
@@ -54,7 +54,7 @@ class EntityGrid
     add entity, entity.position
   end
 
-  requires inside? pos
+  requires inside? pos, entity.template.size
   requires includes? entity
   ensures !includes? entity
   def remove(entity, pos)
@@ -71,7 +71,7 @@ class EntityGrid
   end
 
   requires old != entity.position
-  requires inside? old && inside? entity.position
+  requires inside? old, entity.template.size && inside? entity.position, entity.template.size
   requires includes? entity, old
   ensures includes? entity, entity.position
   def move(old, entity)
@@ -79,8 +79,8 @@ class EntityGrid
     add entity
   end
 
-  def inside?(pos)
-    pos.x >= 0 && pos.y >= 0 && pos.x < @size.x && pos.y < @size.y
+  def inside?(pos, size)
+    pos.x >= 0 && pos.y >= 0 && pos.x + (size.x - 1) < @size.x && pos.y + (size.y - 1) < @size.y
   end
 
   def includes?(entity)
