fa68b5d39815fa632362a70fa57e32552533915d
Groogy <groogy@groogy.se>
2018-08-02 22:10:06 +0200

    Now finds/create social unit properly

--- src/ego/entity/components/social_unit_creator.cr
+++ src/ego/entity/components/social_unit_creator.cr
@@ -1,2 +1,38 @@
 class SocialUnitCreatorComponent < EntityComponent
+  include CrystalClear
+
+  requires entity.has_component? SocialUnitMemberComponent
+  ensures !entity.query SocialUnitMemberComponent, &.owner.nil?
+  def spawn_setup(entity, world)
+    if entity.query SocialUnitMemberComponent, &.owner.nil?
+      unit = find_or_create_unit world, entity
+      unit.register entity
+    end
+  end
+
+  def find_or_create_unit(world, entity)
+    unit = find world, entity
+    unit = create world if !unit
+    unit
+  end
+
+  def find(world, entity)
+    entity = PathFinder.broad_search world, entity.position.point, 10, ->find_unit_at_pos(World, Map::Pos)
+    if entity
+      entity.query SocialUnitMemberComponent, &.owner 
+    else
+      nil
+    end
+  end
+
+  def create(world)
+    world.social_units.create
+  end
+
+  def find_unit_at_pos(world : World, pos : Map::Pos)
+    world.entities.each_at pos do |e|
+      return e if !e.query SocialUnitMemberComponent, &.owner.nil?
+    end
+    nil
+  end
 end
