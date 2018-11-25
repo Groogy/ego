80713bb4e7ee13bb3a5f0b3a6829a10b85fa3e75
Groogy <groogy@groogy.se>
2018-11-25 20:11:36 +0100

    When container is destroyed, destroy its content

--- src/ego/entity/components/base_storage_component.cr
+++ src/ego/entity/components/base_storage_component.cr
@@ -67,6 +67,12 @@ abstract class BaseStorageComponent < EntityComponent
     weight
   end
 
+  def on_destroyed(entity)
+    each do |e|
+      e.destroy
+    end
+  end
+
   abstract def can_store?(storage, entity : Entity)
   abstract def can_store?(storage, tmpl : EntityTemplate)
   abstract def can_take?(storage, entity, taker)
