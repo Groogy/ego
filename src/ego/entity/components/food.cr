b9c7c7d7eb1e8f6e80f1cfbbe80223e41c6af317
Groogy <groogy@groogy.se>
2018-12-08 13:18:32 +0100

    Fixed compile error in food component

--- src/ego/entity/components/food.cr
+++ src/ego/entity/components/food.cr
@@ -2,7 +2,7 @@ class FoodComponent < EntityComponent
   include CrystalClear
 
   def hunger_worth
-    hunger_worth data
+    FoodComponent.hunger_worth @data
   end
 
   def self.hunger_worth(data)
