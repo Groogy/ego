d1125b354442c52fcf2c80ae8ade955bea6059ef
Groogy <groogy@groogy.se>
2019-03-02 19:27:26 +0100

    Tweaked tectonics generation a bit

--- src/ego/tectonics.cr
+++ src/ego/tectonics.cr
@@ -4,17 +4,18 @@ class TectonicLine
   getter points
 
   def generate_shape(random, size)
-    distance_left = random.get_zero_to_one * (size.x + size.y) / 4
+    distance_left = random.get_zero_to_one * (size.x + size.y) / 2
     x, y = random.get_int(0, size.x), random.get_int(0, size.y)
     point = Boleite::Vector2i.new x.to_i, y.to_i
-    angle = Math::PI * random.get_zero_to_one
+    original = angle = Math::PI * 2.0 * (random.get_zero_to_one - 0.5)
     @points = [point]
     while distance_left > 0.0
-      distance = random.get_zero_to_one * 10.0
+      distance = random.get_zero_to_one * 20.0
       distance_left -= distance
-      angle += Math::PI / 4.0 * (random.get_zero_to_one - 0.5)
+      angle += Math::PI / 8.0 * (random.get_zero_to_one - 0.5)
       direction = Boleite::Vector2f.new Math.cos(angle), Math.sin(angle)
       point = point + (direction * distance).to_i
+      break if point.x < 0 || point.y < 0 || point.x > size.x || point.y > size.y
       @points << point
     end
   end
